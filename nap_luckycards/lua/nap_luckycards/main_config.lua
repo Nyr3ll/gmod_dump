b66f27f7e4675dd2f2981dfb7699c086ff887812
Nyrell <99680360+Nyr3ll@users.noreply.github.com>
2025-06-29 14:38:29 +0200

    Delete nap_luckycards directory

--- nap_luckycards/lua/nap_luckycards/main_config.lua
+++
@@ -1,9 +0,0 @@
--- //////////////////////////////////////////////////
--- //////////////    CONFIG    /////////////////////
--- ////////////////////////////////////////////////
-
--- Define default background
-NAP_LUCKYCARDS.Config.LuckyCard.DefaultBackground = "lucky_card_1.png"
-
--- Define default name
-NAP_LUCKYCARDS.Config.LuckyCard.DefaultName = "Carte à gratter"
