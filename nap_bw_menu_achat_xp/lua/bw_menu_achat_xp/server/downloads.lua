f460612a5314344e4e104d3ed2b503b863a2885d
Nyrell <99680360+Nyr3ll@users.noreply.github.com>
2025-06-29 14:38:22 +0200

    Delete nap_bw_menu_achat_xp directory

--- nap_bw_menu_achat_xp/lua/bw_menu_achat_xp/server/downloads.lua
+++
@@ -1,9 +0,0 @@
--- IMAGES
-resource.AddFile("materials/bw_menu_achat_xp/images/close_button_256x256.png")
-resource.AddFile("materials/bw_menu_achat_xp/images/settings_gear_256x256.png")
-resource.AddFile("materials/bw_menu_achat_xp/images/home_256x256.png")
-
--- FONTS
-resource.AddFile("resource/fonts/Comfortaa Regular.ttf")
-resource.AddFile("resource/fonts/Comfortaa Light.ttf")
-resource.AddFile("resource/fonts/Comfortaa Bold.ttf")
